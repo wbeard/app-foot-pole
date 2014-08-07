8a86615ba55a1a5aa54d23e8b207d3fd36e6fbfe
William Beard <wbeard@rallydev.com>
2014-08-07 12:55:37 -0600

    XCode project added

---
+++ app-foot-poleTests/app_foot_poleTests.swift
@@ -0,0 +1,35 @@
+//
+//  app_foot_poleTests.swift
+//  app-foot-poleTests
+//
+//  Created by William Beard on 8/7/14.
+//  Copyright (c) 2014 rallydev. All rights reserved.
+//
+
+import XCTest
+
+class app_foot_poleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
